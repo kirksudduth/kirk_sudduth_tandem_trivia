2c1a7d7ea2674246b1fa17bc82d66bd1adeef2de
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-28 11:00:51 -0500

    adding a menu to the trivia application

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print("*_* WELCOME 2 TRIVIA *_*")
+
+# WELCOME
+def build_menu():
+    print("? ? ? ? ? ? ? ? ? ? ? ? ?")
+    print("? ? WELCOME TO TRIVIA ? ?")
+    print("? ? ? ? ? ? ? ? ? ? ? ? ?")
+    print("")
+    print("1. Start trivia. ")
+    print("2. I need a moment.")
+
+
+def main_menu():
+    build_menu()
+    choice = input(">> ")
+
+
+build_menu()
