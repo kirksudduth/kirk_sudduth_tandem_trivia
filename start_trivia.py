5cca2463f1e36e9a4e24f05e4a1e210068b2b519
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-28 11:37:35 -0500

    got ALL questions displaying with answers in random order

--- start_trivia.py
+++ start_trivia.py
@@ -1,7 +1,14 @@
 import json
-
+import random
 
 with open("data.json") as json_file:
     data = json.load(json_file)
     for d in data:
+        answers = []
+        answers.append(d['correct'])
+        for incorrect in d['incorrect']:
+            answers.append(incorrect)
         print(d['question'])
+        random.shuffle(answers)
+        for a in answers:
+            print("answer: ", a)
