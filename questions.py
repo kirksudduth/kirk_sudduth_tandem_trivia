b8e68c9e20820e95be3a44fcc0505b26613dea11
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-30 13:15:08 -0500

    added the clear terminal function to questions.py and started readme

--- questions.py
+++ questions.py
@@ -1,13 +1,17 @@
 import json
 import time
+import random
+import os
 
 
 def questions_list():
+    os.system('cls' if os.name == 'nt' else 'clear')
     with open("data.json") as json_file:
         data = json.load(json_file)
+        random.shuffle(data)
         for d in data:
             question = d['question']
             print(f'\n{question}')
-            time.sleep(1.5)
+            time.sleep(1)
 
         input("\n\nPress Enter to return to main menu.\n\n")
